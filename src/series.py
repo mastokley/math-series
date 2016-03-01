d088e43abd54b5b9a995a2e64616efc1f469d5bd
Kent Ross <root.main@gmail.com>
2016-03-01 15:28:47 -0800

    worked on tests & functions

--- src/series.py
+++ src/series.py
@@ -2,6 +2,7 @@
 
 
 def fibonacci(n):
+    """Return the nth value of the Fibonacci series for non-negative n"""
     if n == 0:
         return 0
     elif n == 1:
@@ -11,4 +12,53 @@ def fibonacci(n):
 
 
 def lucas(n):
-    pass
+    """Return the nth value of the lucas series for non-negative n"""
+    if n == 0:
+        return 2
+    elif n == 1:
+        return 1
+    else:
+        return lucas(n - 1) + lucas(n - 2)
+
+
+def sum_series(n, first=0, second=2):
+    """Return the nth value of a series that sums the same way as the Fibonacci
+    series, but with different values for the first and second numbers in the
+    series.
+
+    By default, behaves exactly like `fibonacci()`.
+    """
+    if n == 0:
+        return first
+    elif n == 1:
+        return second
+    else:
+        return (
+            sum_series(n - 1, first, second) +
+            sum_series(n - 2, first, second)
+        )
+
+
+if __name__ == "__main__":
+    print("""
+This module defines functions that implement mathematical series.
+
+fibonacci(n):
+    Returns the Fibonacci series at index `n` for non-negative n.
+
+lucas(n):
+    Returns the Lucas series at index `n` for non-negative n.
+
+sum_series(n, first=0, second=1):
+    Returns a sum series similar to the Lucas or Fibonacci series, but with
+    optional user-defined base values for the indexes at 0 and 1.
+
+>>> fibonacci(1)
+1
+>>> fibonacci(7)
+13
+>>> lucas(2)
+3
+>>> sum_series(1, 8, 5)
+43
+""")
--- src/test_series.py
+++ src/test_series.py
@@ -16,6 +16,12 @@ FIB_TABLE = [
 ]
 
 
+@pytest.mark.parametrize('n, result', FIB_TABLE)
+def test_fib(n, result):
+    import series
+    assert series.fibonacci(n) == result
+
+
 LUCAS_TABLE = [
     (0, 2),
     (1, 1),
@@ -30,7 +36,45 @@ LUCAS_TABLE = [
 ]
 
 
-@pytest.mark.parametrize('n, result', FIB_TABLE)
-def test_fib(n, result):
+@pytest.mark.parametrize('n, result', LUCAS_TABLE)
+def test_lucas(n, result):
     import series
-    assert series.fibonacci(n) == result
+    assert series.lucas(n) == result
+
+
+SUM_SERIES_TABLE = [
+    (0, 0, 1, 0),  # fibonacci
+    (1, 0, 1, 1),
+    (2, 0, 1, 1),
+    (3, 0, 1, 2),
+    (4, 0, 1, 3),
+    (5, 0, 1, 5),
+    (6, 0, 1, 8),
+    (7, 0, 1, 13),
+    (8, 0, 1, 21),
+    (9, 0, 1, 34),
+    (0, 2, 1, 2),  # lucas
+    (1, 2, 1, 1),
+    (2, 2, 1, 3),
+    (3, 2, 1, 4),
+    (4, 2, 1, 7),
+    (5, 2, 1, 11),
+    (6, 2, 1, 18),
+    (7, 2, 1, 29),
+    (8, 2, 1, 47),
+    (9, 2, 1, 76),
+    (0, 1, 8, 1),  # something else
+    (1, 1, 8, 8),
+    (2, 1, 8, 9),
+    (3, 1, 8, 17),
+    (4, 1, 8, 26),
+    (5, 1, 8, 43),
+    (6, 1, 8, 69),
+    (7, 1, 8, 112)
+]
+
+
+@pytest.mark.parametrize('n, first, second, result', SUM_SERIES_TABLE)
+def test_sum_series(n, first, second, result):
+    import series
+    assert series.sum_series(n, first, second) == result
